a8d601c38855fb81861cfb974052131436477927
Tyler <toygrrr@gmail.com>
2022-09-22 09:56:14 -0700

    commit tree 1328176a0d2962b14c5bb07aaa941e04dc093ea1

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.string :passenger_id
+      t.string :taxi_id
       t.timestamps
     end
   end
